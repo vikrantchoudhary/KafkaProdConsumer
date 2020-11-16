449d4f8fb285ce9b4d865025533117119c5e5bc8
vikrant_kc <vikrantk@ndtv.com>
2020-11-16 22:05:52 +0530

    Consumer code added.

--- src/main/java/com/vikrant/kafkademo/KafkaProdConsumer/configuration/KafkaConfig.java
+++ src/main/java/com/vikrant/kafkademo/KafkaProdConsumer/configuration/KafkaConfig.java
@@ -1,19 +1,23 @@
 package com.vikrant.kafkademo.KafkaProdConsumer.configuration;
 
 import com.vikrant.kafkademo.KafkaProdConsumer.model.SampleModel;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka //Enable kafka for consumer listener.
 public class KafkaConfig {
     @Bean
     public ProducerFactory<String, SampleModel> producerFactory () {
@@ -28,4 +32,22 @@ public class KafkaConfig {
     public KafkaTemplate<String, SampleModel> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
+
+    @Bean
+    public ConsumerFactory<String,SampleModel> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG,"myConsumerGroupID");
+        return new DefaultKafkaConsumerFactory(config,new StringDeserializer(),new JsonDeserializer<>(SampleModel.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String,SampleModel> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String,SampleModel> concurrentKafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory();
+        concurrentKafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
+        return concurrentKafkaListenerContainerFactory;
+    }
+
 }
--- src/main/java/com/vikrant/kafkademo/KafkaProdConsumer/controller/KafkaPubConController.java
+++ src/main/java/com/vikrant/kafkademo/KafkaProdConsumer/controller/KafkaPubConController.java
@@ -1,10 +1,11 @@
 package com.vikrant.kafkademo.KafkaProdConsumer.controller;
 
+import com.google.gson.JsonObject;
 import com.vikrant.kafkademo.KafkaProdConsumer.model.SampleModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,13 +20,20 @@ public class KafkaPubConController {
         this.kafkaTemplate = kafkaTemplate;
     }
     @PostMapping("/api/kafka/sample")
-    public void post(@RequestBody SampleModel sampleModel) {
+    public void producer(@RequestBody SampleModel sampleModel) {
         kafkaTemplate.send("mytopic", sampleModel);
     }
 
+
     @GetMapping("/api/test")
     public ResponseEntity<String> test () {
-
         return ResponseEntity.ok().body("testing");
+    }
+
+    @KafkaListener(topics = "mytopic")
+    @GetMapping("/api/kafka/consumer")
+    public ResponseEntity<String> consumer(SampleModel sampleModel) {
+        System.out.println(sampleModel.toString());
+        return ResponseEntity.ok().body(sampleModel.toString());
     }
 }
